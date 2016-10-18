b1277910511827141c6282842af2a5d87e059b52
galvani makau futa <r590968@student.thomasmore.be>
2016-10-18 17:44:20 +0200

    applicatie werkbaar maken

--- ex3-makaufuta-galvani/ex3-makaufuta-galvani/main.js
+++ ex3-makaufuta-galvani/ex3-makaufuta-galvani/main.js
@@ -1,4 +1,7 @@
 //variabelen aanmaken
 var express = require('express');//webserver
 var bodyparser = require('body-parser'); //extensie op express
-var app = express();
+var app = express(); //webserver variabel
+
+// applicatie werkbaar maken
+app.use(bodyparser.json()); // automatische json-body parsers van request MET media-type application/json gespecifieerd in de request.
