79d7e0eb95da7167c60b9c9e4ba6dcd6d8244e3d
galvani makau futa <r590968@student.thomasmore.be>
2016-10-18 18:01:30 +0200

    functie defineren

--- ex3-makaufuta-galvani/ex3-makaufuta-galvani/main.js
+++ ex3-makaufuta-galvani/ex3-makaufuta-galvani/main.js
@@ -8,17 +8,17 @@ app.use(bodyparser.json()); // automatische json-body parsers van request MET me
 
 //functie aanmaken
 app.get('/maal/:n1/:n2', function(request, response) {
-    var Antwoord;
+    var Antwoord = parseInt(request.params.n1) * parseInt(request.params.n2);
 }); // functie maal
 
 app.get('/delen/:n1/:n2', function(request, response) {
-    var Antwoord;
+    var Antwoord = parseInt(request.params.n1) / parseInt(request.params.n2);
 }); // functie delen
 
 app.get('/plus/:n1/:n2', function(request, response) {
-    var Antwoord;
+    var Antwoord = parseInt(request.params.n1) + parseInt(request.params.n2);
 }); // functie plus
 
 app.get('/min/:n1/:n2', function(request, response) {
-    var Antwoord;
+    var Antwoord = parseInt(request.params.n1) - parseInt(request.params.n2);
 }); // functie min
