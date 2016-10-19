ec870dc87bf8f8b39faaa4ecc11494c283b62144
galvani makau futa <r590968@student.thomasmore.be>
2016-10-19 12:41:15 +0200

    applicatie bodyparser werkbaar maken

--- ex3-makaufuta-galvani/ex3-makaufuta-galvani/main.js
+++ ex3-makaufuta-galvani/ex3-makaufuta-galvani/main.js
@@ -1,3 +1,7 @@
 //variabelen aanmaken
 var express = require('express'); //webserver
 var bodyparser = require('body-parser'); //extensie op express
+var app = express(); // webserver variabel
+
+//applicatie werkbaar maken
+app.use(bodyparser.json()); //automatische json-body
