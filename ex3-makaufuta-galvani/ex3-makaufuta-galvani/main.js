7b4282db01e2043bcb4fd9be104f268a88762cdb
galvani makau futa <r590968@student.thomasmore.be>
2016-10-19 12:54:46 +0200

    rekenmachine posten op website

--- ex3-makaufuta-galvani/ex3-makaufuta-galvani/main.js
+++ ex3-makaufuta-galvani/ex3-makaufuta-galvani/main.js
@@ -10,7 +10,13 @@ app.use(bodyparser.json()); //automatische json-body
 app.listen(4567); //poort definieren
 console.log("Server started"); //laten verschijnen
 
-//rekenmachine halen uit html
+//rekenmachine halen van html
 app.get("/", function(response){
     response.sendFile(__dirname + '/calculator.html');
+});
+
+//rekenmachine posten op een website
+app.post("/", function(request, response){
+    var calculation = eval (request.body.display);
+    console.log(calculation);
 });
